956a582fca19a5066084f07ef2e70d91c07fa596
kudo210705 <86939262+kudo210705@users.noreply.github.com>
2021-08-05 12:28:39 +0900

    Update playSound.js

--- playSound.js
+++ playSound.js
@@ -6,10 +6,21 @@
     
     // オーディオファイルを登録
     createjs.Sound.registerSound("asserts/sound.mp3", "sound01");
+    
+    var buttons = window.document.getElementsByName("sound01");
+    for (int i = 0; i < buttons.length; i++) {
+      button[i].addEventListener("click", button_onClick);
+    }
   }
   
+  
+  function button_onClick() {
+    createjs.Sound.play("sound01");
+  };
+  
+  /*
   function loadHandler(event) {
     // 音を出す
     createjs.Sound.play("sound01");
-  }
+  }*/
 }(window));
